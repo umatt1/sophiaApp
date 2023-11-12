898f78e34773b0cd5b9f0c3843b1b408bec0a015
Matthew Burkhard <matthewburkhard@Matthews-MacBook-Air.local>
2023-11-11 23:12:11 -0500

    seperates home page to set for router

--- src/App.js
+++ src/App.js
@@ -1,18 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import Home from './Home.js'
 
+// the app consists of a router to other pages containing the pertinent information
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Home />
       </header>
     </div>
   );
---
+++ src/Home.js
@@ -0,0 +1,15 @@
+function Home() {
+    return <div>
+        <h1>A header will go here</h1>
+                <a
+                  className="App-link"
+                  href="https://reactjs.org"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  Learn React
+                </a>
+    </div>;
+}
+
+export default Home
