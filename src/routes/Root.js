d5f36721a873c9b1ba7315279a9aba599a95a409
Matthew Burkhard <matthewburkhard@Matthews-Air.cable.rcn.com>
2023-11-13 15:03:03 -0600

    Fixes formatting

--- src/routes/Root.js
+++ src/routes/Root.js
@@ -4,14 +4,14 @@ function Root() {
     return <div>
       <header className="App-header">
         <h1>A header will go here</h1>
-                <a
-                  className="App-link"
-                  href={`/about`}
-                  //target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  Learn about this project
-                </a>
+        <a
+          className="App-link"
+          href={`/about`}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn about this project
+        </a>
       </header>
     </div>;
 }
